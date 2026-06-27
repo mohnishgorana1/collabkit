614a9f1f1438f1eb500800861463a3b3da3b53eb
Mohnish Gorana <mohnishgorana1@gmail.com>
2026-06-27 15:18:18 +0530

    get full mongo user details fn

--- lib/helpers/auth.ts
+++ lib/helpers/auth.ts
@@ -4,7 +4,7 @@ import User from "@/models/user.model";
 
 export async function getMongoUser() {
   const { userId, sessionClaims } = await auth();
-  
+
   if (!userId) {
     throw new Error("Unauthorized");
   }
@@ -16,13 +16,50 @@ export async function getMongoUser() {
     console.log("⚠️ Fallback: Fetching Mongo ID from DB");
     await dbConnect(); // Ensure DB connection before querying
     const mongoUser = await User.findOne({ clerkId: userId });
-    
+
     if (!mongoUser) {
       throw new Error("User not found in DB");
     }
-    
+
     mongoUserId = mongoUser._id.toString();
   }
 
   return mongoUserId;
+}
+
+export async function getFullMongoUser() {
+  const { userId, sessionClaims } = await auth();
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+
+  let mongoUserId = sessionClaims?.userMongoId as string;
+  let mongoUser;
+
+  // Ensure DB connection before any query
+  await dbConnect();
+
+  if (!mongoUserId) {
+    console.log("⚠️ Fallback: Fetching Mongo ID from DB");
+    const currentUser = await User.findOne({ clerkId: userId });
+
+    if (!currentUser) {
+      throw new Error("User not found in DB");
+    }
+
+    mongoUserId = currentUser._id.toString();
+    mongoUser = currentUser;
+  } else {
+    mongoUser = await User.findById(mongoUserId);
+
+    // Safety check: agar sessionClaims ka ID galat nikla toh clerkId se fetch karo
+    if (!mongoUser) {
+      console.log("⚠️ SessionClaims ID invalid, fallback to clerkId");
+      mongoUser = await User.findOne({ clerkId: userId });
+    }
+  }
+
+  // console.log("mongoUser", mongoUser);
+
+  return JSON.parse(JSON.stringify(mongoUser)); // Safe serialization
 }
